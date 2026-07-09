fb7c7f8cec490d5bac3f5e404b1a9ff79b862cdf
AvadhutVG <avgore2005@gmail.com>
2026-07-09 14:24:30 +0530

    Complete preprocessing pipeline and train baseline linear regression model

--- src/models/prepare_data.py
+++ src/models/prepare_data.py
@@ -14,17 +14,24 @@ def prepare_features(df: pd.DataFrame):
     ]
 
     X = df.drop(columns=columns_to_drop)
+    
+    # Fill missing balcony values
+    X["balcony"] = X["balcony"].fillna(X["balcony"].median())
+
     y = df["price"]
 
     # One-Hot Encode location
     X = pd.get_dummies(
-        X,
-        columns=["location"],
-        drop_first=True,
-        dtype=int
-    )
+    X,
+    columns=["location", "area_type"],
+    drop_first=True,
+    dtype=int
+)
 
     print("\nFeature Matrix Shape:", X.shape)
     print("Target Shape:", y.shape)
+
+    print("\nMissing values in X:")
+    print(X.isnull().sum()[X.isnull().sum() > 0])
 
     return X, y
